9d7bfd6d1d855677127a328dfc87323354bc431a
aluoty <alexanderluo0910@gmail.com>
2026-06-15 20:17:45 +0800

    setup

---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include <raylib.h>
+
+int main() {
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "Emergence");
+    
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Setup", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
